f1596f80d1993cb83b261e6e10ddee5f8beb8b1e
ODudek <dudek.olek4@gmail.com>
2018-03-21 21:39:21 +0100

    first push

---
+++ README.md
@@ -0,0 +1,17 @@
+# Stackedit Desktop
+
+Desktop version of stackedit.io
+
+## How to run
+
+```
+$ npm start
+```
+
+## How to build 
+
+```
+$ npm run package-mac           # Build MacOS package
+$ npm run package-win           # Build Windows package
+$ npm run package-linux         # Build Linux package
+```
--- main.js
+++ main.js
@@ -1,55 +1,193 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const fs = require('fs')
 let win
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+const template = [{
+  label: 'Edit',
+  submenu: [{
+    role: 'undo'
+  },
+    {
+      role: 'redo'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      role: 'cut'
+    },
+    {
+      role: 'copy'
+    },
+    {
+      role: 'paste'
+    },
+    {
+      role: 'pasteandmatchstyle'
+    },
+    {
+      role: 'delete'
+    },
+    {
+      role: 'selectall'
+    }
+  ]
+},
+  {
+    label: 'View',
+    submenu: [{
+      label: 'Reload',
+      accelerator: 'CmdOrCtrl+R',
+      click(item, focusedWindow) {
+        if (focusedWindow) focusedWindow.reload()
+      }
+    },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'resetzoom'
+      },
+      {
+        role: 'zoomin'
+      },
+      {
+        role: 'zoomout'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'togglefullscreen'
+      }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [{
+      role: 'minimize'
+    },
+      {
+        role: 'close'
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [{
+      label: 'Learn More',
+      click() {
+        require('electron').shell.openExternal('https://electron.atom.io')
+      }
+    }]
+  }
+]
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: 'Stackedit',
+    submenu: [{
+      role: 'about'
+    },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'services',
+        submenu: []
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'hide'
+      },
+      {
+        role: 'hideothers'
+      },
+      {
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+    ]
   })
+  // Edit menu.
+  template[1].submenu.push({
+    type: 'separator'
+  }, {
+    label: 'Speech',
+    submenu: [{
+      role: 'startspeaking'
+    },
+      {
+        role: 'stopspeaking'
+      }
+    ]
+  })
+  // Window menu.
+  template[3].submenu = [{
+    label: 'Close',
+    accelerator: 'CmdOrCtrl+W',
+    role: 'close'
+  },
+    {
+      label: 'Minimize',
+      accelerator: 'CmdOrCtrl+M',
+      role: 'minimize'
+    },
+    {
+      label: 'Zoom',
+      role: 'zoom'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Bring All to Front',
+      role: 'front'
+    }
+  ]
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+function createWindow () {
+  win = new BrowserWindow({
+    transparent: true,
+    frame: false,
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+  win.loadURL('https://stackedit.io/app/#')
+  win.webContents.on('did-finish-load', function () {
+    fs.readFile(__dirname + '/assets/styles/style.css', 'utf-8', function (error, data) {
+      if (!error) {
+        var formatedData = data.replace(/\s{2,10}/g, ' ').trim()
+        win.webContents.insertCSS(formatedData)
+      }
+    })
+  })
 
-// Quit when all windows are closed.
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+}
+
+app.on('ready', createWindow)
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
